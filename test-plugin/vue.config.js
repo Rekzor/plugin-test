c1c6c636c1af04e0fbac7b57b0d002fb754ab2d9
David Dizy <david.dizy@arkea.com>
2020-09-23 14:12:03 +0200

    Simple test project with a vue.config.js

---
+++ test-plugin/vue.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  "pluginOptions": {
+    i18n: {
+      locale: 'fr',
+      fallbackLocale: 'fr',
+      localeDir: 'locales',
+      enableInSFC: true
+    },
+    "testOptions": {
+      "test1": 'test',
+      "test2": 'test2'
+    },
+    someOptions: {
+        option1: 'option2',
+        option2: 'option3'
+      }
+  }
+}
